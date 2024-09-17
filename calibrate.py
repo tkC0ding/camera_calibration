702688a1009aa6576e7a2c06310b0767f108f229
tkC0ding <tkjagani@gmail.com>
2024-09-17 15:56:41 +0530

    minor changes

--- calibrate.py
+++ calibrate.py
@@ -2,13 +2,10 @@ import cv2
 import numpy as np
 import os
 
-image_path = 'images/'
-
-images = os.listdir(image_path)
-
 chessboard_size = (9, 6)
 square_size = 3.0 #cm
 image_path = 'images/'
+image_size = (640, 480)
 
 objp = np.hstack([np.mgrid[0:chessboard_size[0], 0:chessboard_size[1]].T.reshape(-1, 2), np.zeros((chessboard_size[0]*chessboard_size[1], 1))])*square_size
 
@@ -21,7 +18,7 @@ for image in images:
     frame = cv2.imread(f'{image_path}{image}')
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    ret, corners = cv2.findChessboardCorners(gray, chessboard_size, None, None)
+    ret, corners = cv2.findChessboardCorners(gray, chessboard_size, None)
 
     if ret:
         object_points.append(objp)
@@ -33,7 +30,11 @@ for image in images:
 
         iamge = cv2.drawChessboardCorners(frame, chessboard_size, corners_final, ret)
         cv2.imshow("points", image)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            continue
+        cv2.waitKey(500)
     else:
         print("no chessboard corners found")
+cv2.destroyAllWindows()
+
+rep, camera_matrix, dist_coeff, rvecs, tvecs = cv2.calibrateCamera(object_points, image_points, image_size, None, None)
+
+np.savez("calibration.npz", camera_matrix=camera_matrix, dist_coeff=dist_coeff, rvecs=rvecs, tvecs=tvecs)
